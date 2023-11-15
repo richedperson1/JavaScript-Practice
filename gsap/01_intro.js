a7c91f578495bf8a8da848a2f593b925bb4fd686
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-11-15 12:09:05 +0530

    adding basic gsap properties

---
+++ gsap/01_intro.js
@@ -0,0 +1,17 @@
+gsap.to("div.animation-box", {
+  x: "500px",
+
+  duration: 1.9,
+  rotation: 360,
+  repeat: -1,
+  yoyo: 1,
+});
+
+// gsap.from("div.animation-box", {
+//   color: "red",
+//   duration: 1.2,
+//   backgroundColor: "green",
+//
+//   x: 800,
+//   yoyo: 1,
+// });
